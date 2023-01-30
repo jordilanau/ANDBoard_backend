74f6badbf1fbe77e2acee92ee8cdb0279f62658f
Jordi <jordi.lanau@and.digital>
2023-01-30 10:57:18 +0000

    Initialize project

---
+++ src/main/java/com/example/andboardbackend/AndBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.andboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndBoardBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AndBoardBackendApplication.class, args);
+  }
+
+}
